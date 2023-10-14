9bcb4132780fd1d9820e0a99aa50df368c4edff1
VladislavTitarenko <vladislav@1worldonline.com>
2023-10-14 15:26:24 +0300

    Create streaming

--- grpc_client.py
+++ grpc_client.py
@@ -3,13 +3,12 @@ import grpc
 import greeting_service_pb2
 import greeting_service_pb2_grpc
 
+
 def run():
-	with grpc.insecure_channel('localhost:8080') as channel:
+    with grpc.insecure_channel('localhost:8080') as channel:
+        stub = greeting_service_pb2_grpc.GreetingServiceStub(channel)
 
-		stub = greeting_service_pb2_grpc.GreetingServiceStub(channel)
-
-		response =stub.greeting(greeting_service_pb2.HelloRequest(name = 'Vlad Python'))
-
-	print("Greeting from server: " + response.greeting)
+        for response in stub.greeting(greeting_service_pb2.HelloRequest(name='Vlad Python')):
+            print("Greeting from server: " + response.greeting)
 
 run()
